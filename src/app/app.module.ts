8bb3f8a4b1a20ca0704c5ad346dc8027b998f5b7
Earl Harris <earleharrisjr@gmail.com>
2022-07-06 19:13:39 -0400

    working on buttons

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { SocialComponent } from './social/social.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    ContactComponent,
+    SocialComponent
   ],
   imports: [
     BrowserModule,
